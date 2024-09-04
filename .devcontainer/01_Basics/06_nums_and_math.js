7ff1d3db5c80da8de6cd44d0569ecacdcb90a402
ShahjamalKhan <94917020+ShahjamalKhan@users.noreply.github.com>
2024-09-04 15:26:41 +0600

    Math and Num

--- .devcontainer/01_Basics/06_nums_and_math.js
+++ .devcontainer/01_Basics/06_nums_and_math.js
@@ -4,3 +4,18 @@ console.log(score);
 
 const balance=new Number(100)
 console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+
+console.log(Math.round(4.6589));
+console.log(Math.ceil(4.6589));
+console.log(Math.floor(4.6589));
+console.log((Math.random()*10)+1);
+
+
