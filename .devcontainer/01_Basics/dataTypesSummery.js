a739b9f55f15de1f430642c7bb8556adfcabbe1e
ShahjamalKhan <94917020+ShahjamalKhan@users.noreply.github.com>
2024-09-04 15:04:27 +0600

    new learning

--- .devcontainer/01_Basics/dataTypesSummery.js
+++ .devcontainer/01_Basics/dataTypesSummery.js
@@ -5,3 +5,13 @@ anothername="Maya";
 console.log(myYoutubename);
 console.log(anothername);
 
+const gameName=new String("Maya");
+
+console.log(gameName);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("y"));
+
+const newString=gameName.substring(0,3)
+console.log(newString);
