9b14a47dfee9ef8783f2fc7fe4f32050fcaa011e
ShahjamalKhan <94917020+ShahjamalKhan@users.noreply.github.com>
2024-09-12 08:16:51 +0600

    Time & Date

---
+++ .devcontainer/01_Basics/07_Dateinjs.js
@@ -0,0 +1,13 @@
+// //Dates
+
+// let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleTimeString());
+
+let myTimeStamp= Date.now();
+console.log(myTimeStamp);
+console.log(myTimeStamp);
+console.log(Date.now());
+
