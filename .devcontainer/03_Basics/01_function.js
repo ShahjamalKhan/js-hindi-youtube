23857899909b0c393fc80dd5897e72816bed6529
ShahjamalKhan <94917020+ShahjamalKhan@users.noreply.github.com>
2024-09-18 01:05:20 +0600

    Function 01

---
+++ .devcontainer/03_Basics/01_function.js
@@ -0,0 +1,42 @@
+
+function calcilateCartPrice(val1,val2,...num1){
+    return num1
+
+
+}
+
+// (... its a rest operator)
+console.log(calcilateCartPrice(200,300,400,500,6000));
+
+
+const user={
+    username: "Shahajamal",
+    price: 99
+}
+
+
+function handleObject(anyObject){
+console.log(`Username is ${anyObject.user} add price is ${anyObject.price}`);
+
+}
+
+// handleObject(user);
+handleObject({
+    username: "sam",
+    price:99
+})
+
+const square=function(x){
+    return x*x;
+}
+
+console.log(square(12));
+
+
+const myNewArray=[200,300,400,500]
+
+function returnSecendValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecendValue(myNewArray));
