4dbb3dfbd83254cdb3fee24e3b355d8322583004
ShahjamalKhan <94917020+ShahjamalKhan@users.noreply.github.com>
2024-09-21 01:19:48 +0600

    if else

---
+++ .devcontainer/04_ControlFlow/one.js
@@ -0,0 +1,56 @@
+// //if
+// const isuserLoggedIn=true
+// const temperature=41
+
+// if(temperature==40){
+//      console.log("TRUE");
+     
+// }else{
+
+// console.log(`Temperature less than 50`);
+// }
+
+
+// const score=200;
+// if(score>100){
+//     const power="fly";
+//     console.log(`User power ${power}`);
+    
+// }
+
+
+
+
+// const balance=1000;
+
+// if(balance>500)
+//     {console.log(`Test`);
+
+//     }
+// if(balance<500)
+//     {
+//         console.log(`Less Than 500`);
+
+//     }else if(balance<750){
+
+//         console.log(`Less Than 750 `);
+//     }
+//     else if(balance<900){
+
+//         console.log(`Less Than 900 `);
+//     }
+
+
+const userLoggedin=true;
+const debitCard=true;
+const loggedInFromGoogle=false
+const loggedInFromEmail=true
+
+if(userLoggedin && debitCard && 2==2){
+    console.log(`Allow to buy courses`);
+    
+}
+if(loggedInFromEmail || loggedInFromEmail){
+    console.log(`User Logged In`);
+    
+}
