ca56dffa304cf7bb0eb98524b2bef264fa072d18
ShahjamalKhan <94917020+ShahjamalKhan@users.noreply.github.com>
2024-09-21 13:27:50 +0600

    basics loop

---
+++ .devcontainer/04_ControlFlow/truthy.js
@@ -0,0 +1,21 @@
+const userEmail="hitesh@gmail.com"
+
+if(userEmail){
+    console.log(userEmail);
+    
+}else{
+    console.log("Dont have the Email");
+    
+}
+
+
+//falsy value
+
+//false, 0,-0, BigInt 0n, "", null, undefined, NaN
+
+//truthy value
+//"0", = truthy value
+
+const iceTeaprice=100
+iceTeaprice<=80? console.log("less than 80"): console.log("more than 80");
+
---
+++ .devcontainer/05_iterations/one.js
@@ -0,0 +1,41 @@
+
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+
+
+// }
+
+// console.log(element);
+
+
+// for (let i = 1; i< 10; i++) {
+//     console.log(`outer loop value ${i}`);
+
+//     for (let j = 0; j <=10; j++) {
+//         console.log(`inner loop value ${j} and inner loop ${i}`);
+//         console.log(i +'*'+j+'='+i*j);
+
+//     }
+
+// }
+
+
+
+/*let myArr = ["flasj", "batman", "superman"]
+for (let index = 0; inderx < myArr.length; index++) {
+    const element = myArr[index];
+    console.log(element);
+    
+}
+    */
+
+
+//Break & Continue
+
+for (let index = 1; index <=20; index++) {
+    console.log(`value of i is ${index}`);
+    
+    
+}
